146b3799413188f03c337592ebf315995c5410ef
Seth Martin <sethgoodluck@gmail.com>
2023-09-11 19:37:47 -0700

    dwm-patch: adds dwm restart signal patch

--- dwm/config.h
+++ dwm/config.h
@@ -17,7 +17,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#008080";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -101,6 +101,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_e,      exitdwm,       {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,          {1} },
 };
 
 /* button definitions */
